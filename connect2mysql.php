8c2b356739942d2f2c08b8db5131caace50688a4
Hitesh Bhagwat <155135951+Hiteshdevss@users.noreply.github.com>
2024-05-04 14:56:45 +0530

    Add files via upload

---
+++ connect2mysql.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "gform"; // change database as per requirements
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+
+?>
