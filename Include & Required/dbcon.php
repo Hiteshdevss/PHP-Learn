28f772253dcd06c3da6a6d4d0e1a64a36777e1ad
Hitesh Bhagwat <155135951+Hiteshdevss@users.noreply.github.com>
2024-05-06 18:27:01 +0530

    Add files via upload

---
+++ Include & Required/dbcon.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "gform";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+else {
+    echo "Connected successfully";
+    echo "<br>";
+}
+
+
+?>
---
+++ Include & Required/include&required.php
@@ -0,0 +1,8 @@
+<?php
+
+//Include & Required
+
+require './dbcon.php';
+
+
+?>
