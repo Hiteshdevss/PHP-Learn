1bee377bf3599d6f4af23f217abd2570fc05275c
Hitesh Bhagwat <155135951+Hiteshdevss@users.noreply.github.com>
2024-05-07 16:01:15 +0530

    fileFunc2.php

--- File IO/fileFunc2.php
+++ File IO/fileFunc2.php
@@ -3,6 +3,12 @@
   fwrite($fptr," lorem ipsum dolor sit amet.\n");
   fwrite($fptr," lorem ipsum dolor sit amet.\n");
   fwrite($fptr," lorem ipsum dolor sit amet.\n");
-  
+
+
+  //Appending to a file in php
+  $fptr = fopen("myfile2.txt", 'a');
+  fwrite($fptr," lorem ipsum dolor sit amet.\n");
+  fwrite($fptr," lorem ipsum dolor sit amet.\n");
+  fwrite($fptr," lorem ipsum dolor sit amet.\n");
   fclose($fptr);
 ?>
